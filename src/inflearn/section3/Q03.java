3055401ed27ee422f5b2a233efd1a5f4d62dd0ff
choejonghyeon <choijonghyen@gmail.com>
2024-05-23 02:35:06 +0900

    [CT-0523] 문제풀이

--- src/inflearn/section3/Q03.java
+++ src/inflearn/section3/Q03.java
@@ -69,7 +69,7 @@ public class Q03 {
         int k = sc.nextInt();
         int[] arr = new int[n];
 
-        for(int i=0; i<k; i++){
+        for(int i=0; i<n; i++){
             arr[i] = sc.nextInt();
         }
 
